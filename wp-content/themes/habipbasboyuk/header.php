5d5145d640db6e40b4e851d632da194b4811548f
basboyukhabip <habibasb@student.arteveldehs.be>
2026-01-26 12:51:21 +0100

    styles for header and hero

--- wp-content/themes/habipbasboyuk/header.php
+++ wp-content/themes/habipbasboyuk/header.php
@@ -6,15 +6,15 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-    <header>
-        <nav>
-            <p><span><</span>HB<span>/></span></p>
-            <ul>
-                <li><a href="#about">About</a></li>
-                <li><a href="#skills">Skills</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li><a href="#hire-me" class="btn-hire">Hire me</a></li>
+    <header class="header">
+        <nav class="header__nav">
+            <p class="header__logo"><span class="header__logo-bracket">&lt;</span>HB<span class="header__logo-slash">/&gt;</span></p>
+            <ul class="header__menu">
+                <li class="header__menu-item"><a href="#about" class="header__menu-link">About</a></li>
+                <li class="header__menu-item"><a href="#skills" class="header__menu-link">Skills</a></li>
+                <li class="header__menu-item"><a href="#projects" class="header__menu-link">Projects</a></li>
+                <li class="header__menu-item"><a href="#contact" class="header__menu-link">Contact</a></li>
+                <li class="header__menu-item"><a href="#hire-me" class="header__menu-link header__menu-link--hire btn-hire">Hire me</a></li>
             </ul>
         </nav>
     </header>
--- wp-content/themes/habipbasboyuk/sections/hero.php
+++ wp-content/themes/habipbasboyuk/sections/hero.php
@@ -1,25 +1,25 @@
-<section>
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/iostab-hero.svg" alt="iostab">
+<section class="hero">
+    <img class="hero__bg-image" src="<?php echo get_template_directory_uri(); ?>/assets/images/iostab-hero.svg" alt="iostab">
 
-    <section>
-        <div>
-            <h1>Hi, I'm habip Basboyuk</h1>
-            <p>Junior Full-stack Developer</p>
-            20 years old, passionate about building good performance apps and websites. I love turning complex problems into scalable solutions.
+    <section class="hero__content">
+        <div class="hero__info">
+            <h1 class="hero__title">Hi, I'm <span>habip Basboyuk</span></h1>
+            <p class="hero__subtitle">Junior Full-stack Developer</p>
+            <span class="hero__desc">20 years old, passionate about building good performance apps and websites. I love turning complex problems into scalable solutions.</span>
 
-            <div>
-                <a href="">Get In touch</a>
-                <a href="">View my work</a>
+            <div class="hero__actions">
+                <a class="hero__action hero__action--contact" href="">Get In touch</a>
+                <a class="hero__action hero__action--work" href="">View my work</a>
             </div>
 
-            <div>
-                <a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/github_icon.svg" alt="Github"></a>
-                <a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/linkedin_icon.svg" alt="LinkedIn"></a>
-                <a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/mail_icon.svg" alt="Mail"></a>
+            <div class="hero__socials">
+                <a class="hero__social hero__social--github" href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/github_icon.svg" alt="Github"></a>
+                <a class="hero__social hero__social--linkedin" href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/linkedin_icon.svg" alt="LinkedIn"></a>
+                <a class="hero__social hero__social--mail" href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/mail_icon.svg" alt="Mail"></a>
             </div>
         </div>
-        <div>
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/habipbasboyuk_hero.png" alt="habip basboyuk">
+        <div class="hero__image-wrapper">
+            <img class="hero__image" src="<?php echo get_template_directory_uri(); ?>/assets/images/habipbasboyuk_hero.png" alt="habip basboyuk">
         </div>
     </section>
 </section>
